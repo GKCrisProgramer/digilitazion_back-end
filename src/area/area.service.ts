a40b41469790b1a78330919ca045997027b684e4
CriCri <salgadoinstitutogk@gmail.com>
2024-12-27 10:58:45 -0700

    CleanCode
    Refactorizaciones pequeñas para aprender

--- src/area/area.service.ts
+++ src/area/area.service.ts
@@ -8,30 +8,25 @@ import { CreateAreaDto } from './DTO/create-area.dto';
 export class AreaService {
     constructor(
         @InjectRepository(Area)
-        private areaRepository: Repository<Area>,
+        private readonly areaRepository: Repository<Area>,
     ) {}
 
-    // Crear una nueva area
     async createDepartment(areaData: CreateAreaDto): Promise<Area> {
-        const {areaName} = areaData;
+        const { areaName } = areaData;
     
-        // Validar si ya existe el departamento
         const existingArea = await this.areaRepository.findOne({ where: { areaName } });
         if (existingArea) {
             throw new BadRequestException('El Area ya existe');
         }
 
-        // Crear el puesto y guardarlo en la base de datos
         const newArea = this.areaRepository.create(areaData);
         return this.areaRepository.save(newArea);
     }
 
-    // Obtener todas las areas
     async findAll(): Promise<Area[]> {
-        return this.areaRepository.find(); // Trae todos los puestos
+        return this.areaRepository.find();
     }
 
-    // Buscar un area por el ID
     async findOne(id: number): Promise<Area> {
         const area = await this.areaRepository.findOne({ where: { areaId: id } });
         if (!area) {
@@ -40,26 +35,23 @@ export class AreaService {
         return area;
     }
 
-    // Eliminar un Area por ID
-    async remove(id: number): Promise<void> {
+    async remove(id: number): Promise<string> {
         const result = await this.areaRepository.delete(id);
         if (result.affected === 0) {
             throw new NotFoundException('Area no encontrado');
         }
+        return 'Area borrada con exito';
     }
 
-    // Actualizar un Area por ID
     async update(id: number, areaData: Partial<Area>): Promise<Area> {
-        const Area = await this.areaRepository.findOne({ where: { areaId: id } });
+        const area = await this.areaRepository.findOne({ where: { areaId: id } });
   
-        if (!Area) {
+        if (!area) {
             throw new NotFoundException('Area no encontrado');
         }
 
-        // Actualiza los datos del Area
-        Object.assign(Area, areaData);
+        Object.assign(area, areaData);
 
-        return this.areaRepository.save(Area);
-        
+        return this.areaRepository.save(area);
     }
 }
