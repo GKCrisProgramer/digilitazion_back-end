1909232487324eec692d3e291736afc5c6b0eee2
CriCri <salgadoinstitutogk@gmail.com>
2025-01-16 08:19:33 -0700

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -62,11 +62,11 @@ import { SeekerController } from './seeker/seeker.controller';
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: process.env.DB_HOST || 'localhost',
-      port: parseInt(process.env.DB_PORT, 10) || 3306,
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
+      host: 'localhost',
+      port: 3306,
+      username: "root",
+      password: "GKCJA5P@",
+      database: "digitalizacionmod2",
       entities: [
         Department,
         DepartmentDocument,
